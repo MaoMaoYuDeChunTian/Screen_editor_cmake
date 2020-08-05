b1ca7f23253f3e1ee75be778358d9a27f2def6f7
zhaoy <zhaoying@gdi.com.cn>
2020-08-05 14:25:05 +0800

    version config

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,11 +1,15 @@
 ﻿#include "ScreenEditor.h"
 #include <QtWidgets/QApplication>
 #include <QTextCodec>
+#include "editor.h.in"
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
 
 	QApplication a(argc, argv);
+	
+	qDebug() << "project_version" << PROJECT_VER;
 
 	ScreenEditor w;
 	w.show();
